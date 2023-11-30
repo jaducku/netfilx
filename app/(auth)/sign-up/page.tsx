474826f800e768b3d804d87c5578d5604ca568e1
jaducku <jaducku@kakao.com>
2023-11-30 23:33:53 +0900

    main & auth

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@/components/ui/input";
+
+export default function SignUp() {
+    return (
+        <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+            <form>
+                Sign Up
+            </form>
+        </div>
+    )
+}
